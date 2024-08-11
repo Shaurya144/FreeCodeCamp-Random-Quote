8cd84d7f8d5b91fb418f7a0f05393179fec531fe
Shaurya144 <chundawatshaurya@gmail.com>
2024-08-11 16:56:58 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const NewQuote = document.getElementById("new-quote");
+const AuthorText = document.getElementById("author");
+
+NewQuote.addEventListener("click", e => {
+  AuthorText.innerHTML("hello");
+})
